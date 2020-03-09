65a7b38525e27b96a319d26840370f2038975212
aebrahim <aebrahim@THQ1LTAEBRAHIM>
2020-03-09 16:36:11 +0300

    100 % of test cases passed

--- HackerRank_Pangrams/Program.cs
+++ HackerRank_Pangrams/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace HackerRank_Pangrams
+class Solution
 {
-    class Program
+
+    // Complete the pangrams function below.
+    static string pangrams(string s)
     {
-        static void Main(string[] args)
-        {
-        }
+        s = s.Replace(" ", "");
+        s = s.ToLower();
+        if (s.Distinct().Count() == 26)
+            return "pangram";
+        else
+            return "not pangram";
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = pangrams(s);
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+
     }
 }
